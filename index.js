dfe3b6c99c547e8d29bf9c42a07546b05d846c72
toto-O <phacusr3tr4@icloud.com>
2023-03-11 18:01:55 +0900

    add variable port number, cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 var app = express();
 
 app.get("/hello", (req, res) => {
+  res.set({ "Access-Control-Allow-Origin": "*" });
   res.json({ message: "hello" });
   res.send("response Express.js");
 });
 
-app.listen(3000, () => {
+const port_number = 3000;
+app.listen(port_number, () => {
   console.log("start server port:3000");
 });
